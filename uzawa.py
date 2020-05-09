429c33ae7e2ff7593039b6a1fe6fede37acaafa5
DCaperaa <damien.caperaa@mines-paristech.fr>
2020-05-09 18:33:21 +0200

    notebook avancé

--- uzawa.py
+++ uzawa.py
@@ -2,26 +2,27 @@
 import numpy as np
 
 A = np.array([[3.0825, 0, 0, 0], [0, 0.0405, 0, 0], [0, 0, 0.0271, -0.0031], \
-     [0, 0, -0.0031, 0.0054]])
+	[0, 0, -0.0031, 0.0054]])
 b = np.array([[2671], [135], [103], [19]])
 C = np.array([[-0.0401, -0.0162, -0.0039, 0.0002], \
-    [-0.1326, -0.0004, -0.0034, 0.0006], [1.5413, 0, 0, 0], \
-    [0, 0.0203, 0, 0], [0, 0, 0.0136, -0.0015], \
-    [0, 0, -0.0016, 0.0027], [0.0160, 0.0004, 0.0005, 0.0002]]).transpose()
+	[-0.1326, -0.0004, -0.0034, 0.0006], [1.5413, 0, 0, 0], \
+	[0, 0.0203, 0, 0], [0, 0, 0.0136, -0.0015], \
+	[0, 0, -0.0016, 0.0027], [0.0160, 0.0004, 0.0005, 0.0002]]).transpose()
 d = np.array([[-92.6], [-29.0], [2671], [135], [103], [19], [10]])
 eps = 1e-8 # paramètre donnant la valeur maximale des composantes
 # de la direction pk pour que pk ne soit pas considérée comme nulle.
 
 def f(p):
-    return float(0.5 * np.matmul(p.transpose(), np.matmul(A, p)) - np.dot(b.transpose(), p))
+	return float(0.5 * np.matmul(p.transpose(), np.matmul(A, p)) - np.dot(b.transpose(), p))
 
 def c(p):
-    return np.matmul(C.transpose(), p) - d
+	return np.matmul(C.transpose(), p) - d
 
 def grad_f(p):
-    return np.matmul(A,p)+b
+	return np.matmul(A,p) + b
+
 def grad_c(p):
-    return C.transpose()
+	return C.transpose()
 
 # Conditions de Wolfe
 def wolfe_step(fun, grad_fun, xk, pk, c1 = 0.25, c2 = 0.75, M = 1000):
@@ -63,7 +64,7 @@ def uzawa_wolfe_step(fun, grad_fun, c, grad_c, x0, rho, lambda0 = 1.0, max_iter 
 		grad_Lagrangienk_xk = grad_Lagrangienk(xk)
 		pk = -grad_Lagrangienk_xk
 		lk = wolfe_step(Lagrangienk, grad_Lagrangienk, xk, pk)
-		xk = xk + lk*pk;        
+		xk = xk + lk*pk;
 		lambdak = np.maximum(0, lambdak + rho*c(xk))
 		k = k + 1
 	print("Nombre d'iterations : ", k)
@@ -71,4 +72,4 @@ def uzawa_wolfe_step(fun, grad_fun, c, grad_c, x0, rho, lambda0 = 1.0, max_iter 
 	return xk
 
 x0=np.array([0,0,0,0])
-print(uzawa_wolfe_step(f,grad_f,c,grad_c,x0,0.1)
+print(uzawa_wolfe_step(f,grad_f,c,grad_c,x0,0.1))
